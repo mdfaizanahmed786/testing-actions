03146824cf6fabca9367b37ce0152077dae4d544
faizan <faizanzaid789@gmail.com>
2022-04-27 01:16:56 +0530

    add ci cd pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Bo it was amazing dude, I liked this so much</code> and save to reload.
         </p>
         <a
           className="App-link"
