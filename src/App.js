acfae13577e06f5c85b23f44ebf871ca00b7a436
faizan <faizanzaid789@gmail.com>
2022-04-27 01:21:59 +0530

    second push

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Bo it was amazing dude, I liked this so much</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hey it was amazing to see you again!
+      I love github actions!
     </div>
   );
 }
